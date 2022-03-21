6a3c9d17eef1376658e68d6b08c31ebd28110371
crunchos <101825643+crunchos@users.noreply.github.com>
2022-03-21 16:09:14 +0200

    Added cool files!
    Oink

---
+++ Hohi/Hohi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hohi
+{
+    public class Hohi
+    {
+        private const string _greetings = "Hello!";
+
+        public override string ToString()
+        {
+            return _greetings;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Hohi h = new Hohi();
+            Console.WriteLine(h);
+        }
+    }
+}
